2f13e2a2fed5d151e092d33b7b184f4efa602f0f
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2024-10-21 16:20:03 -0300

    feat: websocket connection.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,62 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { LinearGradient } from "expo-linear-gradient";
+import { useEffect, useState } from "react";
+import { Text, View } from "react-native";
 
 export default function HomeScreen() {
+  const [temperature, setTemperature] = useState<Number | undefined>();
+  const [isConnected, setIsConnected] = useState(false);
+
+  useEffect(() => {
+    const websocket = new WebSocket("ws://192.168.0.108:3000/ws");
+
+    websocket.onopen = () => {
+      console.log("Opened websocket connection!");
+      setIsConnected(true);
+    };
+
+    websocket.onmessage = (e) => {
+      setTemperature(e.data);
+      console.log(e.data);
+    };
+
+    websocket.onclose = () => {
+      console.log("Closed websocket connection!");
+      setIsConnected(false);
+    };
+  }, []);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View className="flex justify-center items-center h-full">
+      <LinearGradient colors={["#2563EB", "#1E40AF"]} className="rounded-full p-4">
+        <View className="flex items-center justify-center rounded-full p-4 bg-slate-300 h-56 w-56">
+          <Text className="text-4xl text-gray-700">
+            {temperature ? temperature.toString().trim() : "--"} ºC
+          </Text>
+          <Text className="text-gray-700 text-lg mt-3">Temperatura ambiente</Text>
+        </View>
+      </LinearGradient>
+
+      <View className="items-start w-full mt-16 mb-2">
+        <Text className="ml-6">Via Open Weather</Text>
+      </View>
+      
+      <View className="flex-row gap-4">
+        <LinearGradient 
+          colors={["#818cf8", "#6366f1"]}
+          className="justify-center items-center bg-indigo-600 rounded-2xl w-44 h-20 p-2"
+        >
+          <Text className="text-2xl text-zinc-700">30 ºC</Text>
+          <Text className="text-zinc-700 text-lg mt-2">Temperatura atual</Text>
+        </LinearGradient>
+
+        <LinearGradient 
+          colors={["#818cf8", "#6366f1"]}
+          className="justify-center items-center bg-indigo-600 rounded-2xl w-44 h-20 p-2"
+        >
+          <Text className="text-2xl text-zinc-700">60%</Text>
+          <Text className="text-zinc-700 text-lg mt-2">Umidade atual</Text>
+        </LinearGradient>
+      </View>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
